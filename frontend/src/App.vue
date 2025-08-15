0469e1f32e15d7b100846d897bfe6f01fd2db9b5
RickJurrasic <rickjurrasic@gmail.com>
2025-08-15 08:04:07 +0200

    Added Footer.vue component. mobile-design only so far (responsive footer with social icons and copyright)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
-  <NavBar/>
+  <div class="min-h-screen flex flex-col">
+    <NavBar />
+    <main class="flex-1">
+      <!-- Page content will go here -->
+    </main>
+    <Footer />
+  </div>
 </template>
 
 
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="flex items-center justify-around py-5 bg-black">
+    <div class="flex-1 flex justify-center items-center">
+    </div>
+    <div class="flex-1 flex justify-around items-center font-cinzel-decorative">
+      <img :src="facebookIcon" alt="Facebook" class="h-6 w-6" />
+      <img :src="tiktokIcon" alt="TikTok" class="h-6 w-6" />
+      <img :src="instagramIcon" alt="Instagram" class="h-6 w-6" />
+    </div>
+    <div class="flex-1 flex justify-center items-center golden font-cinzel-decorative">
+        <p class="text-xs">Erik Sternad
+            <br>
+            © 2025</p>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import facebookIcon from '../assets/facebook.svg';
+import tiktokIcon from '../assets/tiktok.svg';
+import instagramIcon from '../assets/instagram.svg';
+</script>
+
+<style scoped>
+    footer {
+        background-color: #292929;
+    }
+</style>
